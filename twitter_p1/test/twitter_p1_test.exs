ec8b4e455f8f58fe99b3bf218dc895694deff188
Wins Goyal <winsgoyal.iitj@gmail.com>
2019-11-13 18:19:19 -0500

    Initial file structure for the Project

---
+++ twitter_p1/test/twitter_p1_test.exs
@@ -0,0 +1,8 @@
+defmodule TwitterP1Test do
+  use ExUnit.Case
+  doctest TwitterP1
+
+  test "greets the world" do
+    assert TwitterP1.hello() == :world
+  end
+end
---
+++ twitter_p2/test/twitter_p2_test.exs
@@ -0,0 +1,8 @@
+defmodule TwitterP2Test do
+  use ExUnit.Case
+  doctest TwitterP2
+
+  test "greets the world" do
+    assert TwitterP2.hello() == :world
+  end
+end
