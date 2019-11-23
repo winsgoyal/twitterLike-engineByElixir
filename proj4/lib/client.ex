4e1c67195b79eca3abc591169092fbe8b29ef626
Rahul Wahi <rahulwahi42@gmail.com>
2019-11-23 05:36:27 -0500

    Tweet, Notifications(Mentions and subscriber and update in Login Functionality

--- proj4/lib/client.ex
+++ proj4/lib/client.ex
@@ -10,6 +10,16 @@ defmodule Client  do
   end
   
   def init(state) do
+
+    # this user tweets
+    :ets.new(:tweet, [:set, :protected, :named_table])
+
+     # this user notifications
+    :ets.new(:notification, [:set, :protected, :named_table])
+
+    # this user mentions
+    :ets.new(:mention, [:set, :protected, :named_table])
+
     {:ok, state}
   end
   
@@ -30,69 +40,95 @@ defmodule Client  do
   end
 
   #User will tweet, this function then connect to server
-  def tweet(pid, user) do
-    GenServer.cast(pid, {:tweet, user})
+  def tweet(pid, user, tweet) do
+    GenServer.call(pid, {:tweet, user , tweet })
   end
 
   def subscribe_to(pid, user, subscribe_to_user) do
     GenServer.cast(pid, {:subscribe_to, user, subscribe_to_user})
   end
 
-  # to receive tweet from servers
-  def receive( ) do
-     # code
+ 
+  # this function will receive tweets from server upon login (mostly) and set the user tweets table
+  def receive_tweets(pid , tweets)  do
+    GenServer.cast(pid, {:receive_tweets, tweets})
   end
   
   def set_state(pid, nodeid, routing_table) do  
     GenServer.cast(pid, {:set_state,routing_table,nodeid})
   end
   
-  #to add neighbours to the existing list
-  def handle_cast({:route_to_node,destination_nodeid, hop_count} ,%{routing_table: routing_list, nodeid: node_id } = state) do
-    level = find_level(destination_nodeid,node_id)
-    if destination_nodeid == node_id do
-      
-      NodeInfo.done( hop_count + 1 )
-    else
-      if level == 1 do
-        nodeids = Enum.at(routing_list,0) #search in level1
-        matched_node = Enum.filter(nodeids, fn nodeid -> String.starts_with?(nodeid, String.at(destination_nodeid, 0)) end) 
-        pid = Process.whereis(String.to_existing_atom((Enum.at(matched_node,0)) ))
-       
-        GenServer.cast(pid, {:next_hop,1,destination_nodeid, hop_count + 1})
-      else
-        nodeids = Enum.at(routing_list,1) #search in level2, if common
-        matched_node = Enum.filter(nodeids, fn nodeid -> String.starts_with?(nodeid, String.at(destination_nodeid, 0)) end) 
-        pid = Process.whereis(String.to_existing_atom((Enum.at(matched_node,0)) ))
-        
-        GenServer.cast(pid, {:next_hop,1,destination_nodeid, hop_count + 1})
-      end 
-    end
+  
 
+
+   #it will receive notification and store the tweets in notification
+   # if user is mentioned than stored in mentioned table
+   def handle_cast({:notification, tweet , message } , state ) do
+    IO.puts "Notification" <> message
+    if String.contains?(tweet, "@" ) do
+      :ets.insert_new(:mention, {user, tweets ++ [tweet] })
+    else
+      :ets.insert_new(:notification, {user, tweets ++ [tweet] })
+    end
+    
     {:noreply, state }    
   end
 
+  
+
+  #initialize user tweets, if any received from server
+  def handle_cast({:receive_tweets, tweets} , state ) do
+
+     :ets.insert_new(:tweet, { user, tweets })
+     {:noreply, state }    
+  end
+
+
+  def handle_call( {:tweet, user , tweet }  , state) do
+    
+    {:reply, result, state} = TwitterServer.tweet( user , tweet )
+    if result == "pass" do
+      tweets = :ets.lookup(:subscribe, user)
+      :ets.insert_new(:tweet, {user, tweets ++ [tweet] })
+    else
+      IO.puts "Wither tweet is empty or please try again"
+    end
+    {:reply, state, state, :infinity}  
+  end
+
   def handle_call(:get, _from, state) do
-    {:reply, state, state, 100000}
+    {:reply, state, state, :infinity}
+  end
+
+  def handle_call(:get, _from, state) do
+    {:reply, state, state, :infinity}
   end
   
   def handle_call({:register, user, password}, _from, state) do
     TwitterServer.register( user , password ) ;
-    {:reply,state, state , 100000}
+    {:reply,state, state , :infinity}
   end
 
   def handle_call({:login, user, password}, _from, state) do
-    TwitterServer.login( user, password ) ;
-    {:reply, state, state, 100000}
+    {:reply, result, state} = TwitterServer.login( user, password , self() )
+
+    if result == "pass" do
+      :ets.insert_new(:tweet, {user, []})
+      :ets.insert_new(:notification, {user, []})
+    else
+      IO.puts "Login Failed, Please check Credentials"
+    end
+
+    {:reply, state, state, :infinity}
   end
 
   def handle_cast ({:subscribe_to, user, subscribe_to_user}, _from, state) do
-    TwitterServer.subscribe_user(user, subscribe_to_user})
+    TwitterServer.subscribe_user(user, subscribe_to_user)
     {:noreply, state }
   end
   
   def handle_call(:set, _from, state) do
-    {:reply, state, [], 100000}
+    {:reply, state, [], :infinity}
   end
    
 end
--- proj4/lib/server.ex
+++ proj4/lib/server.ex
@@ -23,6 +23,8 @@ defmodule TwitterServer do
       ## {hashtag, subscribed_by_user_list}
       :ets.new(:hashtag, [:set, :protected, :named_table])
 
+      :ets.new(:pending_notifiaction, [:set, :protected, :named_table])
+
       {:ok, state}
     end
 
@@ -34,8 +36,8 @@ defmodule TwitterServer do
       GenServer.call(__MODULE__, {:register,user, password})
     end
 
-    def login(user , password) do
-      GenServer.call(__MODULE__, {:login,user , password})
+    def login(user , password , pid ) do
+      GenServer.call(__MODULE__, {:login,user , password , pid})
     end
 
     def delete(user) do
@@ -52,7 +54,7 @@ defmodule TwitterServer do
     end
     
     def tweet(user, tweet_text) do
-      GenServer.cast(__MODULE__, {:tweet,user, tweet_text })
+      GenServer.call(__MODULE__, {:tweet,user, tweet_text })
     end
 
     # When user retweet some tweet, then that tweet will appear in
@@ -66,6 +68,94 @@ defmodule TwitterServer do
       GenServer.cast(__MODULE__, {:subscribe_user, user, subscribe_to_user})
     end
 
+    # send notifications to the subscribe users 
+    #user_list is list of users who have subscribed to user tweeter_owner
+    def notify_subscribers( tween_owner , tweet) do
+      GenServer.cast(__MODULE__, {:notify, tween_owner, tweet})
+    end
+
+    # send notifications to the mentioned users 
+    #user_list is list of users who is mentioned in the tweet
+    def notify_mentioned_users( tween_owner , tweet) do
+
+      if String.contains?(tweet, "@" ) do
+        GenServer.cast(__MODULE__, {:notify, tween_owner, tweet})
+      end
+      
+    end
+
+
+    
+
+    #to asend notification to subscriber
+  def handle_cast({:notify_subscribers, tweet_owner, tweet} , _state) do
+    
+    #get list of subscribe by user
+    {tweet_owner, subscribedby} = :ets.lookup(:subscribe, tweet_owner)
+    if length(subscribedby) > 0 do
+      Enum.each( subscribedby, fn user -> 
+        {user,_,_,pid} = :ets.lookup(:user, user)
+        GenServer.cast(pid, {:notification, tweet , tweet_owner <> " has tweeted " })
+      
+      end)
+    end
+    {:noreply, state }    
+  end
+
+    #to asend notification to subscriber
+    def handle_cast({:notify_mentioned_users, tweet_owner, tweet} , _state) do
+    
+      #get list of subscribe by user
+      mentioned_user = Regex.scan(~r/@([a-zA-z0-9]*)/,s)
+      if length(mentioned_user) > 0 do
+        Enum.each( mentioned_user, fn [_,user] -> 
+          {user,_,login_flag,pid} = :ets.lookup(:user, user)
+          if login_flag == 1 do
+            GenServer.cast(pid, {:notification, tweet , tweet_owner <> " has tweeted " })
+          else
+            #add it the pending notification list list
+            #deliver these when users gets back online
+            add_pending_notification( user , tweet , tweet_owner <> " has tweeted " )
+
+          end
+
+        
+        end)
+      end
+      {:noreply, state }    
+    end
+
+    defp add_pending_notification ( user, tweet , message ) do
+      user_pending_notification = :ets.lookup(:pending_notifiaction, user)
+
+      if length(user_pending_notification) > 0 do
+         {user , notifications} = user_pending_notification
+         notifications ++ [[tweet , message]]
+        :ets.insert_new(:pending_notifiaction, {user , notifications} ) 
+      else
+        :ets.insert_new(:pending_notifiaction, {user, [[tweet , message]]}) 
+      end
+    end
+
+    defp send_pending_notification (user) do
+      user_pending_notification = :ets.lookup(:pending_notifiaction, user)
+
+      if length(user_pending_notification) > 0 do
+         {user , notifications} = user_pending_notification
+         
+         if length(notifications) > 0 do
+            Enum.each( notifications, fn [tweet,message] ->  
+              GenServer.cast(pid, {:notification, tweet , message }) 
+            end)
+         end
+        :ets.insert_new(:pending_notifiaction, {user , notifications} ) 
+      else
+        :ets.insert_new(:pending_notifiaction, {user, [[tweet , message]]}) 
+      end
+    end
+
+    end
+
     def handle_call({:register, user, password}, _from, state) do
       
       # existing_usres_map = :ets.lookup(:user, "users")
@@ -74,15 +164,18 @@ defmodule TwitterServer do
       user_details = :ets.lookup(:user, user)
       if length(user_details) > 0 do
         IO.inspect "Username #{user} already exists"
+        {:reply, "fail", state}
       else
-        :ets.insert_new(:user, {user, password, 0})
+        :ets.insert_new(:user, {user, password, 0, 0})
         IO.inspect "Username #{user} Created"
+        {:reply, "pass", state}
       end 
 
-      {:reply, state, state}
+      
     end
 
-    def handle_call({:login, user, password}, _from, state) do
+    #change pid to gproc, as pid can change, if spervisor restarts user process
+    def handle_call({:login, user, password , pid }, _from, state) do
       
       # existing_usres_map = :ets.lookup(:user_lookup, "users")
       # user_details = Map.get( existing_usres_map , user) 
@@ -91,18 +184,28 @@ defmodule TwitterServer do
         # user exists and password matches
         # update the map to have loginflag as 1
         # existing_usres_map = %{ existing_usres_map | user => [password,1] }
-        [ {user, stored_password, _} ] = user_details
+        [ {user, stored_password, _ , _ } ] = user_details
         if stored_password == password do
-          :ets.insert_new(:user, {user, password, 0})
-          IO.inspect "User #{user} Login Successfull"
+          :ets.insert_new(:user, {user, password, 1, pid})
+          intitialize(user)
+          #IO.inspect "User #{user} Login Successfull"
+          Genserver.cast({:receive_tweets, })
+          ## add pending login functionality here 
+          {:reply, "pass", state}
         else
           IO.puts "User name/ password is not correct"
+          {:reply, "fail", state}
         end
       else
         IO.puts "User name/ password is not correct"
+        {:reply, "fail", state}
       end
       
-      {:reply, state, state}
+      
+    end
+
+    defp intialize(user) do
+      # do all the intialization if tables are empty
     end
 
     ## As soon as the user subscribes to another user,
@@ -141,9 +244,26 @@ defmodule TwitterServer do
       end
     end
 
-    def handle_call(:get,_from,state) do
+    def handle_call({:tweet,user, tweet_text } ,_from,state) do
         #IO.puts (Enum.at(list,length(list) -1 ) - start_time)
-        {:reply, state, state}
+
+        if String.length( tweet_text ) > 0 do
+          #get old tweets and so that we can update the list
+          {user, user_tweets} = :ets.lookup(:tweet, user)
+          
+          :ets.insert_new(:tweet, {user, user_tweets ++ [tweet_text] })
+          {:reply, "pass", state}
+        else
+          {:reply, "fail", state}
+        end
+        
     end
+
+    def handle_call(:get,_from,state) do
+      #IO.puts (Enum.at(list,length(list) -1 ) - start_time)
+      {:reply, state, state}
+  end
+
+    
 
 end
