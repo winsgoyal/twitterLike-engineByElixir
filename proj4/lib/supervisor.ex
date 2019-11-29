591d1916ead626388d1153603ae4a58feb9b6592
Wins Goyal <winsgoyal.iitj@gmail.com>
2019-11-29 03:50:09 -0500

    Replaced Genservers with Supervisor in Tests

--- proj4/lib/supervisor.ex
+++ proj4/lib/supervisor.ex
@@ -5,27 +5,25 @@ defmodule MySupervisor do
   use Supervisor
 
   def start_link(init_arg) do 
-     # this user notifications
-     :ets.new(:notification, [:set, :public, :named_table])
+    # this user notifications
+    :ets.new(:notification, [:set, :public, :named_table])
      
-     # this user notifications
-     :ets.new(:client_tweet, [:set, :public, :named_table])
-    Supervisor.start_link(__MODULE__,init_arg)
+    # this user notifications
+    :ets.new(:client_tweet, [:set, :public, :named_table])
+    
+    Supervisor.start_link(__MODULE__, init_arg)
   end
 
   # arg1: numUsers, arg2: numRequest
   def init([arg1, _arg2]) do
     
     children = Enum.map( 1..arg1, fn(n) ->
-      
       worker( Client, [ Integer.to_string(n) ], [ id:  Integer.to_string(n) , restart: :transient, shutdown: :infinity ] )
-
     end )
 
     children = [worker(TwitterServer, [] , [id: TwitterServer, restart: :transient, shutdown: :infinity ])] ++ children
 
     supervise(children, strategy: :one_for_one)
-  
   end
 
 end
--- proj4/test/proj4_test.exs
+++ proj4/test/proj4_test.exs
@@ -4,27 +4,28 @@ defmodule Proj4.TwitterEngineTest do
   doctest Proj4.TwitterEngine
 
   setup_all context do
-    users = ["1", "2", "3"]
-    # client_pids = Enum.map( users, fn user -> Client.start_link(user) )
-    #               |> Enum.map( fn user -> Enum.at(Tuple.to_list(user), 1) )
-    {:ok, pidC1} = Client.start_link("1") # Username: 'user1'
-    {:ok, pidC2} = Client.start_link("2") # Username: 'user2'
-    {:ok, pidC3} = Client.start_link("3") # Username: 'user3'
-    {:ok, pidS} = TwitterServer.start_link()
-    {:ok, [client1: pidC1, client2: pidC2, client3: pidC3, server: pidS]}
+    numUsers = 3
+    numMsgs = 10
+    pidCs = start_processes([numUsers, numMsgs])
+    {:ok, [clients: pidCs]}
+  end
+
+  def start_processes(args) do
+    {:ok, supervisor} = MySupervisor.start_link(args)
+    Enum.map( 1..Enum.at(args, 0), fn user -> Process.whereis(String.to_atom(Integer.to_string(user))) end )
   end
 
   # IF CLIENT PROCESSES WERE CREATED OR NOT
   describe "Creating Clients" do
     test "create first user", context do
       IO.puts "Test 1"
-      assert Process.info(context[:client1], :registered_name) == {:registered_name, :"1"}
+      assert Process.info(Enum.at(context[:clients], 0), :registered_name) == {:registered_name, :"1"}
       IO.puts "Success: User 1 was created\n"
     end
   
     test "create second user", context do
       IO.puts "Test 2"
-      assert Process.info(context[:client2], :registered_name) == {:registered_name, :"2"}
+      assert Process.info(Enum.at(context[:clients], 1), :registered_name) == {:registered_name, :"2"}
       IO.puts "Success: User 2 was created\n"
     end
   end
@@ -48,15 +49,15 @@ defmodule Proj4.TwitterEngineTest do
   describe "carry out User registrations" do
     test "first user registers", context do
       IO.puts "Test 5"
-      Client.register( context[:client1], "1", "user1" )
+      Client.register( Enum.at(context[:clients], 0), "1", "user1" )
       assert :ets.lookup(:user, "1") == [{"1", "user1", 0, 0}]
       IO.puts "Success: User 1 registered, currently logged out\n"
     end
 
     test "two users register", context do
       IO.puts "Test 6"
-      Client.register( context[:client2], "2", "user2" )
-      Client.register( context[:client3], "3", "user3" )
+      Client.register( Enum.at(context[:clients], 1), "2", "user2" )
+      Client.register( Enum.at(context[:clients], 2), "3", "user3" )
       assert :ets.lookup(:user, "2") == [{"2", "user2", 0, 0}]
       assert :ets.lookup(:user, "3") == [{"3", "user3", 0, 0}]
       IO.puts "Success: Both users registered successfully, currently logged out\n"
@@ -64,7 +65,7 @@ defmodule Proj4.TwitterEngineTest do
 
     test "can't create duplicate user", context do
       IO.puts "Test 7"
-      refute Client.register( context[:client2], "2", "user2" ) == "fail"
+      refute Client.register( Enum.at(context[:clients], 1), "2", "user2" ) == "fail"
       IO.puts "Success: User can't create same account twice\n"
     end
   end
@@ -73,176 +74,175 @@ defmodule Proj4.TwitterEngineTest do
   describe "test if the Login/Logout works" do
     test "username is incorrect during login", context do
       IO.puts "Test 8"
-      refute Client.login( context[:client2], "5", "user5" ) == "fail"
+      refute Client.login( Enum.at(context[:clients], 1), "5", "user5" ) == "fail"
       IO.puts "Sucess: The user doesn't exist\n"
     end
 
     test "password is incorrect during login", context do
       IO.puts "Test 9"
-      refute Client.login( context[:client2], "2", "user5" ) == "fail"
+      refute Client.login( Enum.at(context[:clients], 1), "2", "user5" ) == "fail"
       IO.puts "Success: Incorrect password\n"
     end
 
-    ## Giving error on Test 10, 11, 12 -> [error] GenServer :"1" terminating ## Check why and correct.
-    # test "one user logs-in", context do
-    #   IO.puts "Test 10"
-    #   Client.login( context[:client1], "1", "user1" )
-    #   [{_, _, status, _}] = :ets.lookup(:user, "1")
-    #   assert status == 1
-    #   IO.puts "Success: User is logged in now\n"
-    # end
+    test "one user logs-in", context do
+      IO.puts "Test 10"
+      Client.login( Enum.at(context[:clients], 0), "1", "user1" )
+      [{_, _, status, _}] = :ets.lookup(:user, "1")
+      assert status == 1
+      IO.puts "Success: User is logged in now\n"
+    end
 
-    # test "one user logs-out", context do
-    #   IO.puts "Test 11"
-    #   Client.logout( context[:client1], "1" )
-    #   [{_, _, status, _}] = :ets.lookup(:user, "1")
-    #   assert status == 0
-    #   IO.puts "Success: Logged out\n"
-    # end
+    test "one user logs-out", context do
+      IO.puts "Test 11"
+      Client.logout( Enum.at(context[:clients], 0), "1" )
+      [{_, _, status, _}] = :ets.lookup(:user, "1")
+      assert status == 0
+      IO.puts "Success: Logged out\n"
+    end
 
     # test "other/all users login", context do
     #   IO.puts "Test 12"
-    #   Client.login( context[:client1], "1", "user1" )
-    #   Client.login( context[:client2], "2", "user2" )
-    #   Client.login( context[:client3], "3", "user3" )
-    #   assert Enum.map( ["1", "2", "3"], fn username -> Enum.at(Tuple.to_list(:ets.lookup(:user, username)), 2) end ) === [1, 1, 1]
+    #   Client.login( Enum.at(context[:clients], 0), "1", "user1" )
+    #   Client.login( Enum.at(context[:clients], 1), "2", "user2" )
+    #   Client.login( Enum.at(context[:clients], 2), "3", "user3" )
+    #   assert Enum.map( ["1", "2", "3"], fn username -> elem(Enum.at(:ets.lookup(:user, username), 0), 2) end ) == [1, 1, 1]
     #   IO.puts "Success: Three users logged-in\n"
     # end
   end
 
-  describe "Tweeting Starts" do
-    test "", do
-      IO.puts "Test 13"
+  # describe "Tweeting Starts" do
+  #   test "", do
+  #     IO.puts "Test 13"
 
-      IO.puts "Success: "
-    end
+  #     IO.puts "Success: "
+  #   end
 
-    test "", do
-      IO.puts "Test 14"
+  #   test "", do
+  #     IO.puts "Test 14"
 
-      IO.puts "Success: "
-    end
+  #     IO.puts "Success: "
+  #   end
 
-    test "", do
-      IO.puts "Test 15"
+  #   test "", do
+  #     IO.puts "Test 15"
 
-      IO.puts "Success: "
-    end
-  end
+  #     IO.puts "Success: "
+  #   end
+  # end
 
-  describe "Subscribe User's Tweets" do
-    test "", do
-      IO.puts "Test 16"
+  # describe "Subscribe User's Tweets" do
+  #   test "", do
+  #     IO.puts "Test 16"
 
-      IO.puts "Success: "
-    end
+  #     IO.puts "Success: "
+  #   end
 
-    test "", do
-      IO.puts "Test 17"
+  #   test "", do
+  #     IO.puts "Test 17"
 
-      IO.puts "Success: "
-    end
+  #     IO.puts "Success: "
+  #   end
 
-    test "", do
-      IO.puts "Test 18"
+  #   test "", do
+  #     IO.puts "Test 18"
 
-      IO.puts "Success: "
-    end
-  end
+  #     IO.puts "Success: "
+  #   end
+  # end
 
-  describe "Notifications to User when live" do
-    test "", do
-      IO.puts "Test 19"
+  # describe "Notifications to User when live" do
+  #   test "", do
+  #     IO.puts "Test 19"
 
-      IO.puts "Success: "
-    end
+  #     IO.puts "Success: "
+  #   end
 
-    test "", do
-      IO.puts "Test 20"
+  #   test "", do
+  #     IO.puts "Test 20"
 
-      IO.puts "Success: "
-    end
+  #     IO.puts "Success: "
+  #   end
 
-    test "", do
-      IO.puts "Test 21"
+  #   test "", do
+  #     IO.puts "Test 21"
 
-      IO.puts "Success: "
-    end
-  end
+  #     IO.puts "Success: "
+  #   end
+  # end
 
-  describe "Notifications to User when live after disconnect" do
-    test "", do
-      IO.puts "Test 22"
+  # describe "Notifications to User when live after disconnect" do
+  #   test "", do
+  #     IO.puts "Test 22"
 
-      IO.puts "Success: "
-    end
+  #     IO.puts "Success: "
+  #   end
 
-    test "", do
-      IO.puts "Test 23"
+  #   test "", do
+  #     IO.puts "Test 23"
 
-      IO.puts "Success: "
-    end
+  #     IO.puts "Success: "
+  #   end
 
-    test "", do
-      IO.puts "Test 24"
+  #   test "", do
+  #     IO.puts "Test 24"
 
-      IO.puts "Success: "
-    end
-  end
+  #     IO.puts "Success: "
+  #   end
+  # end
 
-  describe "User searches for 'My Mentions'" do
-    test "", do
-      IO.puts "Test 25"
+  # describe "User searches for 'My Mentions'" do
+  #   test "", do
+  #     IO.puts "Test 25"
 
-      IO.puts "Success: "
-    end
+  #     IO.puts "Success: "
+  #   end
 
-    test "", do
-      IO.puts "Test 26"
+  #   test "", do
+  #     IO.puts "Test 26"
 
-      IO.puts "Success: "
-    end
-  end
+  #     IO.puts "Success: "
+  #   end
+  # end
 
-  describe "User searches for Other User" do
-    test "", do
-      IO.puts "Test 27"
+  # describe "User searches for Other User" do
+  #   test "", do
+  #     IO.puts "Test 27"
 
-      IO.puts "Success: "
-    end
+  #     IO.puts "Success: "
+  #   end
 
-    test "", do
-      IO.puts "Test 28"
+  #   test "", do
+  #     IO.puts "Test 28"
 
-      IO.puts "Success: "
-    end
-  end
+  #     IO.puts "Success: "
+  #   end
+  # end
 
-  describe "Users retweet User's tweets" do
-    test "", do
-      IO.puts "Test 29"
+  # describe "Users retweet User's tweets" do
+  #   test "", do
+  #     IO.puts "Test 29"
 
-      IO.puts "Success: "
-    end
+  #     IO.puts "Success: "
+  #   end
 
-    test "", do
-      IO.puts "Test 30"
+  #   test "", do
+  #     IO.puts "Test 30"
 
-      IO.puts "Success: "
-    end
+  #     IO.puts "Success: "
+  #   end
 
-    test "", do
-      IO.puts "Test 31"
+  #   test "", do
+  #     IO.puts "Test 31"
 
-      IO.puts "Success: "
-    end
+  #     IO.puts "Success: "
+  #   end
 
-    test "", do
-      IO.puts "Test 32"
+  #   test "", do
+  #     IO.puts "Test 32"
 
-      IO.puts "Success: "
-    end
-  end
+  #     IO.puts "Success: "
+  #   end
+  # end
 
   
 
